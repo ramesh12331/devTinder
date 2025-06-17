c27bfb51f5dbb62fb873581d9c1c64eaf3bc11e8
ramesh12331 <mamidiramesh503@gmail.com>
2025-06-17 17:41:21 +0530

    wildError-handling

--- src/app.js
+++ src/app.js
@@ -3,24 +3,23 @@ const { adminAuth, userAuth } = require("../middlewares/auth");
 
 const app = express();
 
-app.use("/admin",adminAuth);
-
-app.post("/user/login",(req,res)=>{
-    res.send("User login successfully!");
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        res.status(500).send("Something went wrong!!");
+    }
 })
 
-app.get("/user",userAuth,(req,res)=>{
-    res.send("User Data Send");
-})
-
-app.get("/admin/getAllData",(req,res)=>{
+app.get("/getUserData",(req,res)=>{
+    try {
+        throw new Error("cdfcdc");
+        res.send("User Data Send");
+    } catch (error) {
+        res.status(500).send("Some error contact support team");
+    }
     
-    res.send("All Data Sent")
-});
-
-app.get("/admin/deleteAllData",(req,res)=>{
-    res.send("delete");
 })
+
+
 
 app.listen(5000,()=>{
     console.log("Server is successfully listening on port 5000....");
