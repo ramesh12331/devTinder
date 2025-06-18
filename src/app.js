0c7312a9e4f507574ce94091358d4a81c7d4c948
ramesh12331 <mamidiramesh503@gmail.com>
2025-06-18 22:31:43 +0530

    ValidateSchema

--- src/app.js
+++ src/app.js
@@ -61,6 +61,35 @@ app.get("/feed",async(req,res)=>{
     }
     
     
+});
+
+//DeleteById
+
+app.delete("/user",async(req,res)=>{
+    const userId = req.body.userId;
+    try {
+        const user = await User.findByIdAndDelete({_id:userId});
+        // const user = await User.findByIdAndDelete(userId)
+        res.send("User deleted successfully");
+    } catch (error) {
+        res.status(400).send("Something Went Wrong");
+    }
+
+});
+
+//Update data
+app.patch("/user",async(req,res)=>{
+    const userId = req.body.userId;
+    const data = req.body;
+    
+    try {
+        const user = await User.findByIdAndUpdate({_id:userId},data,{returnDocument : "before",runValidators : true});
+        console.log(user);
+        res.send("User updated Successfully");
+        
+    } catch (error) {
+        res.status(400).send("UPDATE FAILED" + error.message);
+    }
 })
 
 connectDB()
--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,56 @@ const mongoose = require("mongoose");
 
 const userSchema =new mongoose.Schema({
     firstName : {
-        type : String
+        type : String,
+        required : true,
+        minLength : 4,
+        maxLength :100
     },
     lastName : {
         type : String
     },
     emailId : {
-        type : String
+        type : String,
+        required : true,
+        lowercase : true,
+        unique: true,
+        index : true,
+        trim : true
     },
     password : {
-        type : String
+        type : String,
+        required : true
     },
     age : {
-        type : String
+        type : String,
+        min : 18,
+        
     },
     gender : {
-        type : String
+        type : String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("Gender Data is not found");
+            }
+        },
+    },
+    photoUrl : {
+        type : String,
+        default : "https://i.pinimg.com/280x280_RS/79/dd/11/79dd11a9452a92a1accceec38a45e16a.jpg"
+    },
+    about : {
+        type : String,
+        default : "This is a default of the user!",
+    },
+    skills : {
+        type : [String],
     }
-});
+},
+{
+    timestamps:true,
+}
+);
+
+
 
 module.exports = mongoose.model("User",userSchema);
