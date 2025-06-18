7e96e2c7df958e598e7d46a4f93d5b72d0342ebb
ramesh12331 <mamidiramesh503@gmail.com>
2025-06-18 23:06:46 +0530

    API level data sanitisation

--- src/app.js
+++ src/app.js
@@ -78,17 +78,32 @@ app.delete("/user",async(req,res)=>{
 });
 
 //Update data
-app.patch("/user",async(req,res)=>{
-    const userId = req.body.userId;
+app.patch("/user/:userId",async(req,res)=>{
+    const userId = req.params?.userId;
     const data = req.body;
+
+    
     
     try {
+        const ALLOWED_UPDATES = ["photoUrl","about","gender","age","skills"];
+
+    const isUpdateAllowed = Object.keys(data).every((k)=>ALLOWED_UPDATES.includes(k));
+    if(!isUpdateAllowed){
+        throw new Error("Updated not Allowed");
+    }   
+
+    if(data?.skills.length>10){
+        throw new Error("Skills cannot be more than 10");
+    }
+
+    // **************
+
         const user = await User.findByIdAndUpdate({_id:userId},data,{returnDocument : "before",runValidators : true});
         console.log(user);
         res.send("User updated Successfully");
         
     } catch (error) {
-        res.status(400).send("UPDATE FAILED" + error.message);
+        res.status(400).send("UPDATE FAILED :" + error.message);
     }
 })
 
